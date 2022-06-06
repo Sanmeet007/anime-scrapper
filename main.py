b13f71795e3a5d6784b4552f9222a560490198b1
Sanmeet007 <ssanmeet123@gmail.com>
2022-06-06 19:57:53 +0530

    bug fixes

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ scraper = cloudscraper.create_scraper()
 def parse_html():
     with open(FILE, "w+") as f:
         for index, _ in enumerate(range(MAX_EPISODES), start=1):
-            contents = scraper.get(f"{LINK}-{index}").text
+            contents = scraper.get(f"{LINK}-{START_FROM+ index}").text
             html = BeautifulSoup(markup=contents, features="lxml")
             iframe = html.select_one(selector="iframe")
             src = iframe.get("src")
